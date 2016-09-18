8893019ba1064c7adc2e20370ae42f1685110139
Connor Imrie <connorimrie@outlook.com>
2016-09-18 19:08:33 +0100

    Refactored so that annotations are autoloaded when using annotations driver

--- src/Configuration/MetaData/Annotations.php
+++ src/Configuration/MetaData/Annotations.php
@@ -4,16 +4,26 @@
 namespace LaravelDoctrine\ODM\Configuration\MetaData;
 
 
-use Doctrine\ODM\MongoDB\Configuration;
+use Doctrine\ODM\MongoDB\Mapping\ClassMetadataFactory;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 use LaravelDoctrine\ORM\Configuration\MetaData\MetaData;
 
 class Annotations extends MetaData {
 
 	public function resolve(array $settings = [])
 	{
-		return (new Configuration())->newDefaultAnnotationDriver(
-			array_get($settings, 'paths', [])
-		);
+		$driver =(AnnotationDriver::create(array_get($settings, 'paths', [])));
+		AnnotationDriver::registerAnnotationClasses();
+
+		return $driver;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getClassMetadataFactoryName()
+	{
+		return ClassMetadataFactory::class;
 	}
 
 }
---
+++ src/Configuration/MetaData/MetaDataManager.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace LaravelDoctrine\ODM\Configuration\MetaData;
+
+
+use Illuminate\Contracts\Container\Container;
+
+class MetaDataManager extends \LaravelDoctrine\ORM\Configuration\MetaData\MetaDataManager  {
+
+	public function getNamespace()
+	{
+		return __NAMESPACE__;
+	}
+}
